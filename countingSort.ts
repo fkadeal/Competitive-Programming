d29944a12cfddf3d9a558eeda0f0e56782d4a98f
Fkadeal Mat <44930374+fkadeal@users.noreply.github.com>
2022-10-19 09:04:24 -0800

    countingsort1
    https://www.hackerrank.com/challenges/countingsort1/problem

---
+++ countingSort.ts
@@ -0,0 +1,58 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr: number[]): number[] {
+    // Write your code here
+      var newR = [...new Array(100)].map(i=>0) 
+    arr.map((i)=>{
+        if(newR[i] === 0){
+            newR[i]=1
+        }else { newR[i]=newR[i]+1 }
+    }); 
+
+    // console.log();
+    return [...newR]
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result: number[] = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
